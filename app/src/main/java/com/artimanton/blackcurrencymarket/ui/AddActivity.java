9aed1ae5784391aa491de595f192e382eb2f7465
artim-anton <1rimola0>
2020-02-03 13:53:24 +0200

    AddActivity to FireBase

--- app/src/main/java/com/artimanton/blackcurrencymarket/ui/AddActivity.java
+++ app/src/main/java/com/artimanton/blackcurrencymarket/ui/AddActivity.java
@@ -4,12 +4,18 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 
 import com.artimanton.blackcurrencymarket.R;
+import com.artimanton.blackcurrencymarket.model.Record;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class AddActivity extends AppCompatActivity {
+    public EditText etData, etPrice, etKol, etPhone, etKey;
     private FirebaseDatabase database;
     private DatabaseReference reference;
 
@@ -20,9 +26,20 @@ public class AddActivity extends AppCompatActivity {
 
         database = FirebaseDatabase.getInstance();
         reference = database.getReference("advert");
+        etData = (EditText) findViewById(R.id.etData);
+        etPrice = (EditText) findViewById(R.id.etPrice);
+        etKol = (EditText) findViewById(R.id.etKol);
+        etPhone = (EditText) findViewById(R.id.etPhone);
+
     }
 
     public void addBtn(View view) {
-        reference.setValue("Hello, World!");
+        String id = reference.push().getKey();
+        Record newAdvert = new Record(etData.getText().toString(), etPrice.getText().toString(), etKol.getText().toString(), etPhone.getText().toString(), id);
+
+        Map<String, Object> advertValue = newAdvert.toMap();
+        Map<String, Object> record = new HashMap<>();
+        record.put(id, advertValue);
+        reference.updateChildren(record);
     }
 }
